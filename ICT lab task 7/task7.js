7d11e300c072d0848aa9fe513a9b07173c7c5829
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-30 15:34:29 +0500

    Create task7.js

---
+++ ICT lab task 7/task7.js
@@ -0,0 +1,11 @@
+ // Create an object to represent a car, with properties for make, model, and year. Log the
+ // entire object to the console
+
+let car=
+{
+make: "Toyota",
+model: "Corolla",
+year: 2020,
+color: "Black"
+};
+console.log(car);
