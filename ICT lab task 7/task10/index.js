d60f830c2f94cb7e15fe70507ac67a9ef4ffe795
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-30 16:13:09 +0500

    Create index.js

---
+++ ICT lab task 7/task10/index.js
@@ -0,0 +1,5 @@
+// Create a webpage that prompts the user for their feedback on a product and displays an alert
+ // with their feedback. Display an alert that says, "Thank you for your feedback: [feedback]."
+
+let feedback=prompt("Enter Feedback:");
+alert("Thank for your feedback: "+feedback);
