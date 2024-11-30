b794f4df67c0e5b27a1b1fc49dd641da1038874c
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-30 14:58:55 +0500

    Create Task2.js

---
+++ ICT lab task 7/Task2.js
@@ -0,0 +1,10 @@
+ // Create two variables for numbers and perform addition, subtraction, multiplication, and
+ // division. Log the results to the console
+
+let num1 = 10;
+let num2 = 8;
+
+console.log("Division: " + (num1 / num2));
+console.log("Sum: " + (num1 + num2));
+console.log("Product: " + (num1 * num2));
+console.log("Subtraction: " + (num1 - num2));
