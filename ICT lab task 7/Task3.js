9fbc31e7430e7d103877106a751c801759ed8358
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-30 15:00:08 +0500

    Create Task3.js

---
+++ ICT lab task 7/Task3.js
@@ -0,0 +1,12 @@
+ // Write a function that takes a number as an argument and logs whether it is even or odd
+
+function evenodd(num) {
+    if (num % 2 === 0) {
+        console.log(num + " is even");
+    } else {
+        console.log(num + " is odd");
+    }
+}
+
+let n = 8;
+evenodd(n);
