2dadc00e08cdd2cb58905747346dd6605b7fd071
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-30 15:04:30 +0500

    Create Task4.js

---
+++ ICT lab task 7/Task4.js
@@ -0,0 +1,5 @@
+ // Create an array of fruits and log the first and last fruit in the array
+
+let fruits=["Banana","Grape","Apple","Apricot","Orange"]
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1]);
